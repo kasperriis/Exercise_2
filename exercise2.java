6916ee9b018e72f57469f5d07c934bc8b1ca4a4a
kasperriis <kasperm@hotmail.dk>
2022-09-21 15:42:10 +0200

    Update exercise2.java

--- exercise2.java
+++ exercise2.java
@@ -1,6 +1,5 @@
-public class exercise2 {
-	public static void main(String[] args) {
-		
-		System.out.println("Kasper Riis");
-	}
-}
+public class exercise2{
+public static void main(String[] args) {
+	
+	System.out.println("Kasper Riis");
+}}
